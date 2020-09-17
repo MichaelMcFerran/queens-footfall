468b42e44d39800111fed1a094832dad09851f6a
Michael <mmcferran628@qub.ac.uk>
2020-09-17 23:20:23 +0100

    removing container to make more space

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ if(!$resultLog){
         <div class="hero-items owl-carousel">
             <!-- start of building 1 data -->
             <div class="single-hero-item set-bg" data-setbg="img/CSBQueens.jpg">
-                <div class="container">
+                <!-- <div class="container"> -->
                     <div class="hero-text"> 
                         <h4>
                             <?php 
@@ -264,7 +264,7 @@ if(!$resultLog){
 
                         </div>
                     </div>
-                </div>
+                <!-- </div> -->
             </div>
             <!-- end of building 1 data  -->
             <!-- start of building 2 data -->
@@ -291,7 +291,7 @@ if(!$resultLog){
                         <form action="index.php" method='POST' enctype="multipart/form-data">
                             <div class="row">
                                 <div class="col-lg-4">
-                                    <select name="RoomBuilding1">
+                                    <select name="RoomBuilding">
 
                                     <?php
                                     $roomlist = "SELECT roomName FROM FMRooms WHERE buildingName = '$name'";
@@ -336,9 +336,55 @@ if(!$resultLog){
                                         <!-- gets progress from DB table, then displays -->
                                         <?php
                                         if(isset($_POST['postB'])){
+                                            //if no date is given, display todays date values as result
+                                            if(empty($_POST['date'])){
+                                                //add variables for all posted data
+                                                $room =$conn->real_escape_string($_POST['RoomBuilding']);
+                                                //default date to current if not specified
+                                                $currentdate = date("Y-m-d");
+                                                //already have building ID
+
+                                                //automatically getting roomID based on selection made, could auto do building but number must be auto gen from moment new carousel created
+                                                $roomID = "SELECT DISTINCT roomID FROM `FMRooms` WHERE roomName = '$room'" ;
+                                                $roomIdResult = $conn->query($roomID);
+                                                if(!$roomIdResult){
+                                                    echo $conn->error;
+                                                } 
+                                                else {
+                                                        while($row2=$roomIdResult->fetch_assoc()){
+                                                        
+                                                            //var names = row of data with explicit dB row name used
+                                                            $currentroomId =$row2['roomID']; 
+                                                        }
+                                                    }
+                                                //db entry
+                                                $selectToday = "SELECT * FROM FMusers WHERE RoomID = '$currentroomId' AND BuildingID = '$buildingId' AND `Time` BETWEEN '$currentdate 00:00:00' AND '$currentdate 23:59:59' ORDER BY `Time` DESC LIMIT 3";
+                                                //db query
+                                                $todayResult = $conn->query($selectToday);
+                                                if(!$todayResult){
+                                                    echo $conn->error;
+                                                } else {
+                                                //must be name of result to check dB data on top, fetchs data
+                                                    while($row5=$todayResult->fetch_assoc()){
+                                                        
+                                                        //var names = row of data with explicit dB row name used
+                                                        $currentF =$row5['CurrentFootfall']; 
+                                                        $Times =$row5['Time'];   
+                                                        // change room ID to name  !  
+                                                    //now echo to display vars with fetched data from dB
+                                                    echo "
+                                                    
+                                                    <tr>
+                                                    <td>$room</td>
+                                                    <td>$currentF</td>
+                                                    <td>$Times</td>
+                                                    </tr>";
+                                                    }
+                                                }
+                                            }
                                             //add variables for all posted data
                                             //added for security against SQL injections
-                                            $room =$conn->real_escape_string($_POST['RoomBuilding1']);
+                                            $room =$conn->real_escape_string($_POST['RoomBuilding']);
                                             $dateOriginal =$conn->real_escape_string($_POST['date']);
                                             $replaceddate = str_replace("/", "-", $dateOriginal);
                                             $newDate = date("Y-m-d", strtotime($replaceddate));
@@ -427,7 +473,7 @@ if(!$resultLog){
                                     <form action="index.php" method='POST' enctype="multipart/form-data">
                                         <div class="row">
                                             <div class="col-lg-4">
-                                                <select name="RoomBuilding1">
+                                                <select name="RoomBuilding">
 
                                                 <?php
                                                 // $roomlist = "SELECT roomName FROM FMRooms WHERE buildingName = '$name'";
@@ -472,9 +518,55 @@ if(!$resultLog){
                                                     <!-- gets progress from DB table, then displays -->
                                                     <?php
                                                     // if(isset($_POST['postB'])){
+                                                    // //if no date is given, display todays date values as result
+                                                    // if(empty($_POST['date'])){
+                                                    //     //add variables for all posted data
+                                                    //     $room =$conn->real_escape_string($_POST['RoomBuilding']);
+                                                    //     //default date to current if not specified
+                                                    //     $currentdate = date("Y-m-d");
+                                                    //     //already have building ID
+
+                                                    //     //automatically getting roomID based on selection made, could auto do building but number must be auto gen from moment new carousel created
+                                                    //     $roomID = "SELECT DISTINCT roomID FROM `FMRooms` WHERE roomName = '$room'" ;
+                                                    //     $roomIdResult = $conn->query($roomID);
+                                                    //     if(!$roomIdResult){
+                                                    //         echo $conn->error;
+                                                    //     } 
+                                                    //     else {
+                                                    //             while($row2=$roomIdResult->fetch_assoc()){
+                                                                
+                                                    //                 //var names = row of data with explicit dB row name used
+                                                    //                 $currentroomId =$row2['roomID']; 
+                                                    //             }
+                                                    //         }
+                                                    //     //db entry
+                                                    //     $selectToday = "SELECT * FROM FMusers WHERE RoomID = '$currentroomId' AND BuildingID = '$buildingId' AND `Time` BETWEEN '$currentdate 00:00:00' AND '$currentdate 23:59:59' ORDER BY `Time` DESC LIMIT 3";
+                                                    //     //db query
+                                                    //     $todayResult = $conn->query($selectToday);
+                                                    //     if(!$todayResult){
+                                                    //         echo $conn->error;
+                                                    //     } else {
+                                                    //     //must be name of result to check dB data on top, fetchs data
+                                                    //         while($row5=$todayResult->fetch_assoc()){
+                                                                
+                                                    //             //var names = row of data with explicit dB row name used
+                                                    //             $currentF =$row5['CurrentFootfall']; 
+                                                    //             $Times =$row5['Time'];   
+                                                    //             // change room ID to name  !  
+                                                    //         //now echo to display vars with fetched data from dB
+                                                    //         echo "
+                                                            
+                                                    //         <tr>
+                                                    //         <td>$room</td>
+                                                    //         <td>$currentF</td>
+                                                    //         <td>$Times</td>
+                                                    //         </tr>";
+                                                    //         }
+                                                    //     }
+                                                    // }
                                                     //     //add variables for all posted data
                                                     //     //added for security against SQL injections
-                                                    //     $room =$conn->real_escape_string($_POST['RoomBuilding1']);
+                                                    //     $room =$conn->real_escape_string($_POST['RoomBuilding']);
                                                     //     $dateOriginal =$conn->real_escape_string($_POST['date']);
                                                     //     $replaceddate = str_replace("/", "-", $dateOriginal);
                                                     //     $newDate = date("Y-m-d", strtotime($replaceddate));
