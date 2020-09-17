ed2afa7a1b03535ff2baeff387e106b86ae20566
Michael <mmcferran628@qub.ac.uk>
2020-09-17 22:49:01 +0100

    test

--- index.php
+++ index.php
@@ -153,7 +153,7 @@ if(!$resultLog){
                                         <!-- gets progress from DB table, then displays -->
                                         <?php
                                         // used to check if post is null/empty and display values for today as default
-                                         if(empty($_POST['postB'])){
+                                         if(null($_POST['postB'])){
                                             //add variables for all posted data
                                             $currentdate = date("Y-m-d");
                                             //already have building ID
@@ -229,38 +229,6 @@ if(!$resultLog){
                                                 }
                                             }
                                         } 
-                                        //doesn't work
-                                        else {
-                                            // //       //add variables for all posted data
-                                            // $currentdate = date("Y-m-d");
-                                            // //already have building ID
-
-                                            // //db entry
-                                            // $selectToday = "SELECT * FROM FMusers WHERE BuildingID = '$buildingId' AND `Time` BETWEEN '$currentdate 00:00:00' AND '$currentdate 23:59:59' ORDER BY `Time` DESC LIMIT 3";
-                                            // //db query
-                                            // $todayResult = $conn->query($selectToday);
-                                            // if(!$todayResult){
-                                            //     echo $conn->error;
-                                            // } else {
-                                            // //must be name of result to check dB data on top, fetchs data
-                                            //     while($row=$todayResult->fetch_assoc()){
-                                                    
-                                            //         //var names = row of data with explicit dB row name used
-                                            //         $roomID = $row['RoomID'];
-                                            //         $currentF =$row['CurrentFootfall']; 
-                                            //         $Times =$row['Time'];   
-                                            //         // change room ID to name  !  
-                                            //     //now echo to display vars with fetched data from dB
-                                            //     echo "
-                                                
-                                            //     <tr>
-                                            //     <td>$room</td>
-                                            //     <td>$currentF</td>
-                                            //     <td>$Times</td>
-                                            //     </tr>";
-                                            //     }
-                                            // }
-                                        }
                                         ?>
 
                                     </tbody>
