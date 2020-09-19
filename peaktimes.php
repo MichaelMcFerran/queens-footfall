0a216bf029c97c11418d612c8e1b91413154474e
Michael <mmcferran628@qub.ac.uk>
2020-09-19 15:48:10 +0100

    test

--- peaktimes.php
+++ peaktimes.php
@@ -18,7 +18,7 @@ include('conn.php'); //changed to /
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Peak Footfall Queens</title>
     <!-- Css -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
    
@@ -59,7 +59,7 @@ include('conn.php'); //changed to /
         <div class="hero-items owl-carousel">
             <!-- start of selected room data -->
             <div class="single-hero-item set-bg" data-setbg="#">
-                <div class="container-fluid">
+                <div class="container">
                     <div class ="row">
                         <div class="hero-text">
                             <h4> PHP here for building name</h4> 
@@ -68,9 +68,9 @@ include('conn.php'); //changed to /
                     </div>
                     <div class="row">
                         <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                            <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
-                        <div class="progress">
+                        <!-- <div class="progress">
                         <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                         <div class="progress">
@@ -96,7 +96,7 @@ include('conn.php'); //changed to /
                         </div>
                         <div class="progress">
                         <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
+                        </div> -->
                     </div>
                     <!-- end of row  gathering all hours of specified day and their peak counts  -->
                 </div>
@@ -111,8 +111,11 @@ include('conn.php'); //changed to /
 
 
     <!-- Js Plugins -->
-    <script src="/js/jquery-3.3.1.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
+    <!-- <script src="/js/jquery-3.3.1.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="/js/jquery.slicknav.js"></script>
     <script src="/js/owl.carousel.min.js"></script>
     <script src="/js/main.js"></script>
